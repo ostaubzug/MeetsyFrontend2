1c5b9280feb24ca2057534bc8eebe3d789795a48
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-23 16:03:13 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import App from './App.vue'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options:{
+            prefix: 'p',
+            darkModeSelector: false
+        }
+
+    }
+});
+
