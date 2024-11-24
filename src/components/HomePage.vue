59db86365663aa54f0b225e0e4d00757fb1d14ad
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-24 14:30:42 +0100

    applied simple styling

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,20 +1,26 @@
 <script>
 
 import Button from 'primevue/button';
+import Image from 'primevue/image';
+
 
 export default {
   name: "HomePage",
   components: {
-    Button
+    Button,
+    Image
   }
 }
 </script>
 
 <template>
-  <div class="flex flex-nowrap">
+  <div class="flex flex-col items-center md:flex-row">
+
+    <Image src="/src/assets/Meetsy.png" alt="Image" width="550" class=""/>
+
     <h1 class="text-9xl text-stone-700" >meetsy</h1>
-    <h2>We help you meet your friends easy</h2>
-    <Button label="Click me" severity="Danger" rounded raised/>
+    <h2 class="text-3xl text-stone-500 my-6">We help you meet your friends easy</h2>
+    <Button label="Get inspiration" severity="Danger" class="mt-10" rounded raised/>
 
   </div>
 </template>
