6dc096fc9c6c72cced40755113f19a99e55c8a69
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-24 15:28:17 +0100

    improve styling

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -15,10 +15,8 @@ export default {
 
 <template>
   <div class="container mx-auto">
-    <div class="flex flex-col items-center md:flex-row">
-
+    <div class="flex flex-col items-center md:flex-row min-h-screen">
       <Image src="/src/assets/Meetsy.png" alt="Image" width="550" class=""/>
-
       <div class="flex flex-col items-center md:items-start">
         <h1 class="text-9xl text-stone-700">meetsy</h1>
         <h2 class="text-3xl text-stone-500 my-6 text-center">We help you meet your friends easy</h2>
