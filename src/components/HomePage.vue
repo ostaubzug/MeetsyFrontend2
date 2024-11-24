e6d86edcade07f3d9301019847cb9929e830bda7
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-24 15:44:20 +0100

    added scroller

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -9,7 +9,19 @@ export default {
   components: {
     Button,
     Image
+  },
+  methods: {
+    scrollToSection(sectionId) {
+      const element = document.getElementById(sectionId)
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      }
+    }
   }
+
 }
 </script>
 
@@ -20,9 +32,12 @@ export default {
       <div class="flex flex-col items-center md:items-start">
         <h1 class="text-9xl text-stone-700">meetsy</h1>
         <h2 class="text-3xl text-stone-500 my-6 text-center">We help you meet your friends easy</h2>
-        <Button label="Get inspiration" severity="Danger" class="mt-10" rounded raised/>
+        <Button label="Get inspiration" severity="Danger" class="mt-10" rounded raised @click="scrollToSection('events')"/>
       </div>
+    </div>
 
+    <div class="min-h-screen" id="events">
+      Hello
     </div>
   </div>
 </template>
