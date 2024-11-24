c75db75a4b8fdb348c80623c1ba83ee7f42076bf
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-24 10:13:22 +0100

    install theming

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
-<script setup>
+<script>
+
+import Button from 'primevue/button';
+
+export default {
+  components: {
+    Button
+  }
+}
 </script>
 
 <template>
   <h1>Hello World</h1>
+
+  <Button label="Click me" />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,32 @@ import App from './App.vue'
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import {definePreset} from "@primevue/themes";
+
+
+const MeetsyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
+
 
 const app = createApp(App);
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: MeetsyPreset,
         options:{
             prefix: 'p',
             darkModeSelector: false
@@ -16,4 +37,6 @@ app.use(PrimeVue, {
 
     }
 });
+
+app.mount('#app');
 
