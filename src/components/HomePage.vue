00e285ba9230d40f7b001a380885b8726421b631
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-24 14:55:26 +0100

    items are aligned

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -14,14 +14,18 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-col items-center md:flex-row">
+  <div class="container mx-auto">
+    <div class="flex flex-col items-center md:flex-row">
 
-    <Image src="/src/assets/Meetsy.png" alt="Image" width="550" class=""/>
+      <Image src="/src/assets/Meetsy.png" alt="Image" width="550" class=""/>
 
-    <h1 class="text-9xl text-stone-700" >meetsy</h1>
-    <h2 class="text-3xl text-stone-500 my-6">We help you meet your friends easy</h2>
-    <Button label="Get inspiration" severity="Danger" class="mt-10" rounded raised/>
+      <div class="flex flex-col items-center md:items-start">
+        <h1 class="text-9xl text-stone-700">meetsy</h1>
+        <h2 class="text-3xl text-stone-500 my-6 text-center">We help you meet your friends easy</h2>
+        <Button label="Get inspiration" severity="Danger" class="mt-10" rounded raised/>
+      </div>
 
+    </div>
   </div>
 </template>
 
