476fbf604c8a5e7800af1102916be6b3bc4a7094
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-11-28 17:50:11 +0100

    added secon message bubble type

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,8 +1,10 @@
 <script>
 import MessageBubbleRight from "@/components/MessageBubbleRight.vue";
+import MessageBubbleLeft from "@/components/MessageBubbleLeft.vue";
 export default {
   name: "EventList",
   components: {
+    MessageBubbleLeft,
     MessageBubbleRight
   }
 }
@@ -12,6 +14,8 @@ export default {
   <div class="container mx-auto min-h-screen">
     <div id="events" class="pt-5">
       <MessageBubbleRight message="Good Morning this is some test message. i try to make this message a bit longer than normal to see how it will behave"/>
+      <MessageBubbleLeft message="Good Morning this is some test message. i try to make this message a bit longer than normal to see how it will behave"/>
+
     </div>
 
   </div>
---
+++ src/components/MessageBubbleLeft.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: "MessageBubbleLeft", // Changed name to reflect position
+  props: {
+    message: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container m-5 mt-10 max-w-xl md:justify-center">
+    <div class="chat-bubble-container">
+      <div class="chat-bubble">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.chat-bubble-container {
+  display: flex;
+  justify-content: flex-start;  /* Changed from flex-end to flex-start */
+}
+
+.chat-bubble {
+  max-width: 60%;
+  padding: 10px 15px;
+  background-color: #0479ff;
+  color: white;
+  border-radius: 20px;
+  position: relative;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.4;
+  margin: 10px 0;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.chat-bubble::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 30px;  /* Changed from right to left */
+  width: 0;
+  height: 0;
+  border: 10px solid transparent;
+  border-top-color: #0479ff;  /* Changed to match bubble color */
+  border-bottom: 0;
+  margin-bottom: -10px;
+}
+</style>
